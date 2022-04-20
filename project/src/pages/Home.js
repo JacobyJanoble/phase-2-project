7aaa706beaaa89dde4d75225ae77e964c6b62aa1
Jacob <Jacob.McCann214@gmail.com>
2022-04-20 13:18:16 -0500

    src components and pages created

---
+++ project/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import NavBar from '../components/NavBar'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Announcements />
+        <NavBar/>
+        <Slider />
+        <Categories />
+    </div>
+  )
+}
+
+export default Home
